afc9d4b3b2b12b3bfbfd7016cbc5cc743f3642cb
swfsql <swfsql@users.noreply.github.com>
2020-08-09 22:23:10 +0000

    deploy: f0eebd6e2c9ffe901099273718beebdbd2f16d43

---
+++ implementors/druid/data/trait.Data.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["ovrt_sys"] = [{"text":"impl Data for <a class=\"struct\" href=\"ovrt_sys/types/struct.Uid.html\" title=\"struct ovrt_sys::types::Uid\">Uid</a>","synthetic":false,"types":["ovrt_sys::types::Uid"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
